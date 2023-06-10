b622f09dda57858327035883c3b445607bcb78a8
Tri <trimpham98@gmail.com>
2023-06-10 06:55:41 -0700

    Change text

--- src/script.js
+++ src/script.js
@@ -32,7 +32,7 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
   const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
 
   // Text
-  const textGeometry = new TextGeometry("Hello Three.js", {
+  const textGeometry = new TextGeometry("Tri Pham Go Live", {
     font: font,
     size: 0.5,
     height: 0.2,
